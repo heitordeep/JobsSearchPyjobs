f4da3af374370cda5f5f8722c70b1272068af4aa
Fernando Heitor Lopes <heitor.aguia@gmail.com>
2020-08-21 10:15:14 -0300

    Fix error database

--- crawl_jobs/crawl_jobs/database_setup.py
+++ crawl_jobs/crawl_jobs/database_setup.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine
 
 from crawl_jobs.models import Base
 
-engine = create_engine(config('DATABASE'), echo=True)
+engine = create_engine('sqlite:///db.sqlite', echo=True)
 Base.metadata.create_all(engine)
